7eadac930386b7129d1c42e89a97b6d07ee2494d
Shiva Kumar <skumar@mobeewave.com>
2020-05-18 02:23:42 -0400

    Modified existing tests

--- src/conftest.py
+++ src/conftest.py
@@ -1,24 +1,45 @@
 import pytest
 import requests
+import json
+
+version = "V3"
+
 
 @pytest.fixture(scope="session")
 def url():
-    version='V3'
-    return 'https://virginia-integration-hivecore.mobeewave-hive.com/api/'+version
+    return 'https://virginia-integration-hivecore.mobeewave-hive.com/api/' + version
+
 
 @pytest.fixture(scope="session")
-def get_auth_token():
+def mpos_url():
+    return 'https://virginia-integration-mposcore.mobeewave-hive.com/api/' + version
 
-    endpoint = 'https://virginia-integration-hivecore.mobeewave-hive.com/api/V3/authentication/hive/login'
+
+@pytest.fixture(scope="session")
+def get_mpos_auth_token():
+    mpos_endpoint = 'https: // virginia-integration-mposcore.mobeewave-hive.com/api/v3/authentication/mpos/login'
+    payload = {
+        "merchantAccountNumber": "0018175",
+        "email": "shivakumar.mobeewave@gmail.com",
+        "password": "Mobeewave2015"
+    }
+    return get_auth_token(mpos_endpoint, payload)
+
+
+@pytest.fixture(scope="session")
+def get_hiveCore_auth_token():
+
+    hive_endpoint = 'https://virginia-integration-hivecore.mobeewave-hive.com/api/V3/authentication/hive/login'
     payload = {
         "username": "convergeadmin",
         "password": "Mobeewave2015"
     }
+    return get_auth_token(hive_endpoint, payload)
+
+
+def get_auth_token(endpoint, payload):
     response = requests.post(endpoint, json=payload)
-    status = response.status_code
-    body=response.json()
-    new=body.items()
-    keys =list(new)
-    auth_token = keys[1][1]['tokens']['accessToken']
-    #print(auth_token)
+    assert response.status_code == 200
+    auth_token = json.loads(response.text)['payload']['tokens']['accessToken']
+    print(auth_token)
     return auth_token
--- src/test_hive_core.py
+++ src/test_hive_core.py
@@ -2,22 +2,31 @@ import requests
 import pytest
 import json
 
+# Auth param not required
 
 
-def test_health_check(get_auth_token,url):
-   a = url+'/healthcheck/hive/status'
-   print(a)
-   response = requests.get(a, headers={'Authorization': get_auth_token})
-   assert response.status_code == 200
+def test_health_check(get_hiveCore_auth_token, url):
+    endpoint = url+'/healthcheck/hive/status'
+    print(endpoint)
+    response = requests.get(
+        endpoint, headers={'Authorization': get_hiveCore_auth_token})
+    assert response.status_code == 200
+
+# Auth param not required
 
 
-def test_full_server_status(get_auth_token,url):
-   #b = url('/healthcheck/hive/fullServerStatus')
-   b= url+'/healthcheck/hive/fullServerStatus'
-   print(b)
-   response = requests.get(b, headers={'Authorization': get_auth_token})
-   payload =response.json()
-   new = payload.items()
-   keys = list(new)
-   error = keys[2]
-   assert error == ('error', None)
+def test_full_server_status(get_hiveCore_auth_token, url):
+    endpoint = url+'/healthcheck/hive/fullServerStatus'
+    response = requests.get(
+        endpoint, headers={'Authorization': get_hiveCore_auth_token})
+    error = json.loads(response.text)['error']
+    assert error == 'None' and response.status_code == 200
+
+
+def test_end_to_end_scenario_1(mpos_url, get_mpos_auth_token):
+    endpoint = mpos_url+''
+    response = requests.get(
+        endpoint, headers={'Authorization': get_mpos_auth_token})
+
+
+# assert error == None
